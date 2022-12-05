f5ca84175b8688695a4416f1f4ba3a6fe8d7f45b
AkshatDaxini <akshatdaxini1@gmail.com>
2022-12-05 12:05:31 -0500

    UI chnages

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -40,28 +40,7 @@ const About = () =>{
           </p>
         </div>
 
-        <div className="stage-cube-cont">
-          <div className="cubespinner">
-            <div className="face1">
-              <img src={pythonlogo} alt="python"  height = "150px" width="150px"/>
-            </div>
-            <div className="face2">
-              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-            </div>
-            <div className="face3">
-              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-            </div>
-            <div className="face4">
-              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-            </div>
-            <div className="face5">
-              <FontAwesomeIcon icon={faJava} color="red" />
-            </div>
-            <div className="face6">
-              <FontAwesomeIcon icon={faAndroid} color="green" />
-            </div>
-          </div>
-        </div>
+        
       </div>
       <Loader type="pacman" />
       </>
