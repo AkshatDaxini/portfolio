cd29c1c4f0f230f0e5f0423618c1de6fc3f48c81
AkshatDaxini <akshatdaxini1@gmail.com>
2022-12-08 21:58:05 -0500

    Worked on contact page

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -4,11 +4,26 @@ import projects from './projects.json';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDesktop } from '@fortawesome/free-solid-svg-icons';
+import { useState, useEffect } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
+
 const Project = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000)
+    }, [])
 
     return (
         <>
+            <div className="heading">
+                <h1>
+                    <AnimatedLetters letterClass={letterClass} strArray={['M', 'y', ' ', 'W', 'o', 'r', 'k']} index={15} />
+                </h1>
+            </div>
             <div className="container-project-page">
+
                 {projects.map(element => {
                     return (
                         <div className='project-card'>
@@ -17,20 +32,19 @@ const Project = () => {
                                 <span className='project-name'><a href={element.git}>{element.name}</a></span><br />
                                 <span className='lang-used'>{element.lang}</span>
                                 <div className='project-description'> {element.desc}</div><br />
-
                             </div>
                             <div className="btn">
-                                
-                            <center>
-                            {element.git !== null&&<a target="_blank" rel="noreferrer" href={element.git}>
-                                    <button><FontAwesomeIcon icon={faGithub} color="#4d4d4e" /> Soruce</button>
-                                </a>}
-                               
-                                {element.demo !== null&&<a target="_blank" rel="noreferrer" href={element.demo}>
-                                    <button><FontAwesomeIcon icon={faDesktop} color="#4d4d4e" /> Demo</button>
-                                </a>}
-                            </center>
-                              
+
+                                <center>
+                                    {element.git !== null && <a target="_blank" rel="noreferrer" href={element.git}>
+                                        <button><FontAwesomeIcon icon={faGithub} color="#4d4d4e" /> Soruce</button>
+                                    </a>}
+
+                                    {element.demo !== null && <a target="_blank" rel="noreferrer" href={element.demo}>
+                                        <button><FontAwesomeIcon icon={faDesktop} color="#4d4d4e" /> Demo</button>
+                                    </a>}
+                                </center>
+
                             </div>
                         </div>
                     )
